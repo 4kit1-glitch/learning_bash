836ec89ecc039012df62d3ab883c2faa6a627753
Kengah Ireneaus <4jrkit1@gmail.com>
2026-05-21 13:20:51 +0100

    printing name and colour

--- base_scripts/script1.sh
+++ base_scripts/script1.sh
@@ -10,5 +10,14 @@ echo the current directory is $PWD
 
 echo i will now set some variables
 
-myname = "john"
-colour = "black"
+myname="john"
+colour="black"
+
+echo "i will give you your prompt back"
+
+echo "i will now fetch you the list of current users"
+w 
+echo 
+
+echo "i am now using the variables above"
+echo "my name is $myname and my favourite colour is $colour"
