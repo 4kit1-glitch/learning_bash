f271b581cbf2f05b9772afd0bc49d29ce96b9389
Kengah Ireneaus <4jrkit1@gmail.com>
2026-05-21 01:25:36 +0100

    Add additional Bash learning script

---
+++ base_scripts/script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear 
+
+echo the script starts now
+
+# this is a comment in the script
+echo hello $USER 
+echo the current directory is $PWD
+
+echo i will now set some variables
+
+myname = "john"
+colour = "black"
