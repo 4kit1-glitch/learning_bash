2d6d7e75bd3bbad84717c27f54fd0e0633ebc66c
Kengah Ireneaus <4jrkit1@gmail.com>
2026-05-22 08:24:52 +0100

    comments in bash

---
+++ base_scripts/commented-script1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# this file shows the use of comments in bash scripts
+# it is a commented version of script1.sh
+
+clear       # clears the shell screen
+
+echo "the script starts now"       # introduction statement
+
+echo hello $USER        # greeting the user
+
+myname="john"       # create a variable named john
+color="black"       # create a variable
+
+echo "i will fetch you a list of current users"
+w       # list of current users loogs in with some info 
+echo    # a space
+
+echo "take back your prompt $PWD"   #printing current working directory
+
+
+
