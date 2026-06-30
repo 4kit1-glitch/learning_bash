d67dfa711c161de7d5d363ee5fb135d399cc40bf
Kengah Ireneaus <4jrkit1@gmail.com>
2026-06-30 16:18:26 +0100

    answered first two exercises

---
+++ exercises/exercise1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# program answers exercises from chapter 1
+
+echo "which folde is bash found"
+echo "answer = $(find /bin/bash)"
+echo "what is the version of bash your using"
+echo "answer = $(bash --version)"
