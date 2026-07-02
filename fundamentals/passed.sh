1ee290bd0946c7c4d8b852ca1730602e3e50f26b
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-02 21:33:33 +0100

    FIX BUGS

--- fundamentals/passed.sh
+++ fundamentals/passed.sh
@@ -6,9 +6,9 @@ echo "script reads parameters passed and prints them"
 
 ## this makes use of the $* special parameters to get and print the values
 
-echo First parameter $1
-echo Second parameter $2
-echo Third parameter $3
+echo "First parameter $1"
+echo "Second parameter $2"
+echo "Third parameter $3"
 
 ## these parameters can be passed unto variables
 
@@ -18,7 +18,10 @@ third_name=$3
 
 full_name=$*    # this makes a string of all parameters passed (more like options when the variable is found)
 
-echo your full name is: $full_name
+echo "your full name is: $full_name"
+
+
+# the value of the IFS VARIABLE CAN AFFECT THE 
 
 
 
