135dadef503b22d5bc2d37520e629a5f515fab3d
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-02 22:10:44 +0100

    learn $@, $#, $*

---
+++ fundamentals/variables/specialP/par2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+
+# the $@ is very similanr to the $* the only difference lies whrn they are in ("") Double quotes
+
+# 1. @ the value of the parameters are treated separately its able to detect strings and it is also more safer
+
+echo "Program prints parameters passed"
+
+for var in "$@"; do
+    echo "var= $var"
+done
+
+echo "There are $# parameters passed"
