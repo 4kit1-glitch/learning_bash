cc1e2345a7c51a2cd6749af32e9951f80db1c41d
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-02 11:59:18 +0100

    learning variables in bash

---
+++ fundamentals/variables/shadowing.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# shadowing refers to when a local variable with the same name as a global variable hides the 
+# effect of the global variable inside a function
+# use the local keyword cause without it variables are global by default
