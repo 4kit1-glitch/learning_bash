713a93504161adf900aceedb7166eeec19c3a0de
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-02 12:32:49 +0100

    added summary using echo

--- exercises/credencials.sh
+++ exercises/credencials.sh
@@ -9,5 +9,7 @@ age=17
 marital_status="single"
 
 # summary
-
 printf "My name is %s, i was born in %s, i am %d years old and i am %s\n" "$name" "$place_of_birth" "$age" "$marital_status"
+
+# summary with echo
+echo my name is $name, i was born in $place_of_birth, i am $age years old and i am $marital_status
