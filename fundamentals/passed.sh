841840b4f0a1ea305cf4c28aea1f232c5bd50770
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-02 21:32:02 +0100

    modified to see full name

--- fundamentals/passed.sh
+++ fundamentals/passed.sh
@@ -10,7 +10,15 @@ echo First parameter $1
 echo Second parameter $2
 echo Third parameter $3
 
+## these parameters can be passed unto variables
 
+first_name=$1
+second_name=$2
+third_name=$3
+
+full_name=$*    # this makes a string of all parameters passed (more like options when the variable is found)
+
+echo your full name is: $full_name
 
 
 
