2b83a2437e2de8f1721333547fde0b46a08eef32
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-02 21:27:34 +0100

    learn to take parameters

---
+++ fundamentals/passed.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+echo "script reads parameters passed and prints them"
+
+## this makes use of the $* special parameters to get and print the values
+
+echo First parameter $1
+echo Second parameter $2
+echo Third parameter $3
+
+
+
+
+
