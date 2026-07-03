039991567e12b1cc9750eec0beac6fc287f8534c
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-03 17:02:26 +0100

    other parameters

---
+++ fundamentals/variables/specialP/others.sh
@@ -0,0 +1,12 @@
+#!/bin//bash
+
+clear
+
+# the $0 Gives the way you ran the script not nessesarily the script name
+echo " this is the current script: $0"      # the $0 parameter expands to the name or path of the current shell script
+
+## if you run with the full path it will print the full path
+## if you run with relative path it will give relative path
+## if you just give the script name it will give just the script name - it shows hot you ran the script
+
+
