1692dce142cbd3e787af63485b0d83d9789b55e7
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-04 23:46:06 +0100

    learn brace expansion

---
+++ fundamentals/expansion/brace_exp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Brace expansion 
+
+echo created file{1,2,3}.txt   # remember no space between the braces and it doesnt work with quotes
+
+echo runs immediately after tokenization phase
+
