c30170f3cda2897cd5935342c569edec80f99850
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-04 13:26:25 +0100

    Give custom instructions to user

--- fundamentals/variables/backup.sh
+++ fundamentals/variables/backup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-# Script creates a backup of the home folder
+set -euo pipefail   # for catching errors 
 
-clear
+# This script makes a saved list of all directories and files in a given location
+# also keeps a log of the output by counting the lenght of the output using python
+
+echo "$(basename $0) <folder name or path>"
+
+# variables 
+folder_name=""
+
+printf "Script starts now"
+
+
+# Create a timestamp in a logfile.
+date >> $LOGFILE
+echo backup succeeded >> $LOGFILE
