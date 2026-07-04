cf254e651854e0abf28e6018bfff80d8b3bbd843
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-04 13:43:19 +0100

    first version

--- fundamentals/variables/backup.sh
+++ fundamentals/variables/backup.sh
@@ -5,14 +5,10 @@ set -euo pipefail   # for catching errors
 # This script makes a saved list of all directories and files in a given location
 # also keeps a log of the output by counting the lenght of the output using python
 
-echo "$(basename $0) <folder name or path>"
 
 # variables 
-folder_name=""
+DIR=$1
 
-printf "Script starts now"
+echo "$(basename $0) <$DIR>"
 
-
-# Create a timestamp in a logfile.
-date >> $LOGFILE
-echo backup succeeded >> $LOGFILE
+ls -la $DIR > list.txt
