c9e9e5627688364b633230b4d411e14a342c8172
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-05 17:42:35 +0100

    learn tilde expansion

--- fundamentals/comments/dirstack.sh
+++ fundamentals/comments/dirstack.sh
@@ -3,14 +3,32 @@
 ## this script demonstrates how working with the dirstack
 
 echo "see items in dirstack"
-dir
+dirs    # see the dirstack
 echo
 
 echo "add videos directory"
 pushd ~/Videos
+echo
 
-echo "see the dirstack"
-dir
+
 echo "current working directory = $PWD" ## this should show videos
 
+echo "see items in dir stack = $(dirs)"
 
+echo "add pictures directory"
+pushd ~/Pictures
+
+echo "see items in dir stack = $(dirs)"
+echo ~+  ## see pwd
+
+
+popd 
+
+echo ~+
+echo ~-
+dirs
+
+
+cd ~1   ## moves to first item on the dir stack and removes everyitem before it 
+pwd
+dirs
