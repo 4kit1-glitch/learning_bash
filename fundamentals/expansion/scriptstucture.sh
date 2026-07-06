f6f324c9269a12a3324c25f054843fffd4b91f85
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-06 15:43:11 +0100

    learn param expansion

---
+++ fundamentals/expansion/scriptstucture.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash             ## script intro always
+
+
+# script description
+
+
+#all ways use pipefail for professional scripting
+set -euo pipefail   # step 2 with any call
+
+## default configs, Global variables , Global constants 
+# for constants use CAPS_PYTHON_CASE then for all other use lower case
+# local variables follow the same rule
+
+
+# functions and operations
