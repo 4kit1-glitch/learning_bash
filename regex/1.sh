0cb4c7052e5cc4f171615d8eed9ce9f37f8c45c6
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-10 19:38:19 +0100

    leaning regex

--- regex/1.sh
+++ regex/1.sh
@@ -1,6 +1,16 @@
 #!/bin/env bash
 
+# progam studies regex using the rm.txt file which is a kit summary 
+# program uses the grep so read "man grep" 
 set -euo pipefail
 
+# 1. the use of anchors ^  and $
 
-grep
+
+cd regex    # making sure am in my folder
+# find the lines that start with -
+grep -E "^-" rm.txt
+
+
+
+
