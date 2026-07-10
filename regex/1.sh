eb3b5853e98ee634276602988413215dbd8aa2cc
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-10 19:41:25 +0100

    leaning regex

--- regex/1.sh
+++ regex/1.sh
@@ -2,15 +2,21 @@
 
 # progam studies regex using the rm.txt file which is a kit summary 
 # program uses the grep so read "man grep" 
+
+# DONT RUN THIS SCRIPT
 set -euo pipefail
 
 # 1. the use of anchors ^  and $
 
 
 cd regex    # making sure am in my folder
+
 # find the lines that start with -
 grep -E "^-" rm.txt
 
+
+# finding all words that end in .
+grep -E 
 
 
 
