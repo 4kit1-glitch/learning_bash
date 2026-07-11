3ea047b6676c8005bd281726fb162a594748e63d
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-11 15:35:12 +0100

    learning sed editing

--- sed/script1.sh
+++ sed/script1.sh
@@ -1,10 +1,24 @@
 #!/bin/env bash
 
 # script demonstrates interactive editing in sed
+# to run remove the clear statements one by one 
 set -euo pipefail
 
 # printing lines containing a file patterns
 cd ~+/sed   # move to directory of file
 cat -n config.txt # see full file numbered
+
+clear
+
+# this command prints the whole file and prints where the pattern matches twice
+sed '/erors/p' config.txt
+
+clear
+
+# to ensure on ly the pattern we want matches we use the 
+sed -n  '/erors/p' config.txt
+
+
+
 
 
