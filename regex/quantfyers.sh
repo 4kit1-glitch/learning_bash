51929963ec2ee2043a1c56003b598232511d2577
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-12 12:50:17 +0100

    learning sed editing

--- regex/quantfyers.sh
+++ regex/quantfyers.sh
@@ -3,7 +3,18 @@
 set -euo pipefail
 
 # Quantifyers  specify how many times to match 
+# quantifyers attach to the thing in the left of it 
+
 cd ~+/regex
 clear
 
 grep -Ei 't?T?heme' req.info
+
+clear
+
+## exactly the use of ?, + , * have been shown 
+
+## now lets move to {n}
+
+# lines with exactly 4 digits
+grep -Eo '[0-9]{2,4}' req.info
