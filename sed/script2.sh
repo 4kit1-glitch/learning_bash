d512a5ef9deff8740b5fb7a0e273d402f1a0b9a3
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-12 11:35:31 +0100

    learning sed editing

--- sed/script2.sh
+++ sed/script2.sh
@@ -2,8 +2,17 @@
 
 # learning to use sed commands in linux
 # find and replace in sed
+
 set -euo pipefail
+cd ~+/sed
 
 # you use the s/ tag infront of the pattern 
 ## example 
-### sed 's/old/new/' ## the old and new can be whole regex patterns  
+### sed 's/old/new/' ## the old and new can be whole regex patterns 
+
+clear
+# replace the first occurance
+sed 's/erors/errors/' config.txt 
+
+# the above command makes it such that sed checks only the first occurence
+# but if we are to make it such that 
