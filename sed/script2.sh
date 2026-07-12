898449bd0c385e511a5fd4b18996a1742ad77b8d
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-12 11:13:21 +0100

    learning sed editing

---
+++ sed/script2.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+# learning to use sed commands in linux
+# find and replace in sed
+set -euo pipefail
+
+# you use the s/ tag infront of the pattern 
+## example 
+### sed 's/old/new/' ## the old and new can be whole regex patterns  
