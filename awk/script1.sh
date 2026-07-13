e1bcc101694959fd967476746faf3f4b9b263240
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-13 17:31:53 +0100

    learn gawk programming language

--- awk/script1.sh
+++ awk/script1.sh
@@ -9,3 +9,9 @@ df -h | grep -v "^Filesystem" | awk '{print $1 " size =" $2}' | sed '/none/d' # 
 clear
 ## sorting by size
 df -h | sort -rnk 5 | head -3 | awk '{print "Partition: " $6 "\t: " $5 " warning!!!"}'
+
+clear
+## regex in awk
+## syntax awk '/pattern/ { action }' file
+
+awk '//'
