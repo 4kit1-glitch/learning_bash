b0e4b07e56911babefe5d222961f280180241848
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-13 16:33:45 +0100

    learn gawk programming language

--- awk/script1.sh
+++ awk/script1.sh
@@ -6,3 +6,6 @@ set -euo pipefail
 
 df -h | grep -v "^Filesystem" | awk '{print $1 " size =" $2}' | sed '/none/d' # simple formatting with awk and sed and grep
 
+clear
+## sorting by size
+df -h | sort -rnk 5 | head -3 | awk '{print "Partition: " $6 "\t: " $5 " warning!!!"}'
