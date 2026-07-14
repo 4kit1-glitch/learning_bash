c233abc4be007ec572c8e0a55ba06f887f39faf1
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-14 21:20:47 +0100

    leanr conditionals

--- conditionals/script1.sh
+++ conditionals/script1.sh
@@ -5,7 +5,24 @@ cd /home/kit/Kitstdios/learning_bash/conditionals
 set -euo pipefail
 
 # test if a file exists
-if [ -a print.c ]; then
+if [ -a print.c ]; then # avoid this in new scripts have other meaning 
     echo "print file exist"
-    cat print.c;
+fi
+
+clear
+# test if file exist and is a directory
+if [ -d "/home/kit/Kitstdios/learning_bash/conditionals" ]; then
+    echo "print file exist and is a dir"
+fi
+
+clear
+# correct way to test if file exist
+if [ -e print.c ]; then
+    echo "file exist well done";
+fi
+
+clear
+# check if file is regular and exist
+if [ -f print.c ]; then
+    echo "file is regular";
 fi
