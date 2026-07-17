8e380481080ad08e84f0d17781dd3e5be8bfed41
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-17 07:39:07 +0100

    learn conditionals

---
+++ conditionals/script5.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+## this script covers using case statements in bash
+# general rule ::
+# case Expression in 
+# case1) 
+    # what to do
+#    ;;
+## esac
+# must end with an esac
