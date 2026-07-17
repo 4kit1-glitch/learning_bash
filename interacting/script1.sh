d4d803fdb211b53eb381402dea8ae3efe55af229
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-17 12:18:04 +0100

    learn to get user input

--- interacting/script1.sh
+++ interacting/script1.sh
@@ -1,3 +1,31 @@
 #!/usr/bin/env bash
 
-# this script explains writing pinterractive scripts -- larger and more effective scripts
+# this script explains writing pinterractive scripts -- larger and more effective scriptsCMD
+
+## catching users input
+# this is done with the read built in command
+# read [options] name1 name2 -- nameN
+
+#------------------- read built in method----------------------
+read -p "Enter your name" name  # standard method with a prompt
+
+echo "hey $name"
+
+clear
+
+# ----------------- output with delim
+read -d "." -p "Enter the message: " message
+
+clear
+
+echo -e "you said \n- $message"
+
+clear
+
+# ------------------- read with readline command or -e tag-----------------------
+read -e -p "hello : " greet
+echo "$greet"
+
+clear
+#--------------------- time out
+
