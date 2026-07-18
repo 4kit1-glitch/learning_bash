6e977b692b1bd990c67c0dc3c3f3cd397913cb29
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-18 16:21:10 +0100

    learn looping in bash

--- repetitions/script2.sh
+++ repetitions/script2.sh
@@ -3,15 +3,26 @@
 ## looping with range functions -- somehow
 
 # python style looping {start..end}
-for i in {1..10}; do 
+for i in {1..10}; do    # this uses brace expansion so using vars wont work cuause of execution sequencing
     echo $i
 done
 
 clear
 
 ## c - slyle looping 
-for (( i = 0; i < 10; i++ )); do 
+for (( i = 0; i < 10; i++ )); do # variables works here well
     echo "hello $i"
 done
 
 ## using variables to loop
+
+clear
+start=1
+stop=10
+step=2
+
+for i in $(seq $start $stop); do
+    echo "hello world"
+done
+
+# or just use (( ))
