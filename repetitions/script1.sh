622d82fc82d600cf2de421a33b613db0ebb65720
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-18 04:20:27 +0100

    learn looping in bash

---
+++ repetitions/script1.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# script show the basic of repition and looping in bash
+
+#1 for -loops 
+## used to iterate over a list of items or a range of numbers. The syntax is as follows:
+# 
+# for name in list; do 
+#       commands;
+# done
+
+# if list is not set then by default it will iterate over the positional parameters passed to the script or function. i.e "$@"
+
+# ----------------------------- -------------------------------------------------------------------------------------
