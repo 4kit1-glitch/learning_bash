106f646ce99f602fa10527c86e8356808df6c457
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-21 10:14:25 +0100

    learn loops

---
+++ repetitions/select2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+run_menu()  {
+    options=("option1" "option2" "option3" "Exit")
+
+    PS3="please choose and option: "
+
+    select option in ${options[@]}; do 
+        case $opt in
+            "Option 1")
+                echo "You selected Option 1"
+                ;;
+            "Option 2")
+                echo "You selected Option 2"
+                ;;
+            "Option 3")
+                echo "You selected Option 3"
+                ;;
+            "Exit")
+                echo "Exiting..."
+                break
+                ;;
+            *)
+                echo "Invalid choice. Please enter a number between 1 and 4."
+                ;;
+        esac
+    done
+}
+
+run_menu
