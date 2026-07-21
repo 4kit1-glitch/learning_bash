6320c3523cf0729673b0bc6ef46a104f86e28c98
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-21 16:21:59 +0100

    learn advanced variables

--- Advancedvars/arrays.sh
+++ Advancedvars/arrays.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-# script demonstrates creation and use of array variables 
+# script demonstrates creation and use of array variables
+
+
+#-------------------creation---------------------------------
+declare -a indexed_array    # creating an indexed arrar
+declare -A associative_array # creation of associative array
+
