1b83412e880ee4b7a135872c8715ef415e9947aa
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-21 16:45:07 +0100

    learn advanced variables

--- Advancedvars/arrays.sh
+++ Advancedvars/arrays.sh
@@ -14,4 +14,5 @@ ARRAY=("name1" "name2" "name3") # this creates an auto indexed array
 # access indexed array elements using index
 echo "First element of indexed array: ${ARRAY[0]}"
 
-# access
+# access all elements with the @ or the *
+echo -e "all elements \n${ARRAY[@]}"
