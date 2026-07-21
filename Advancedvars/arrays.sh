7d762a00c0f83005a5ed1b4251afd92e0894d498
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-21 16:30:18 +0100

    learn advanced variables

--- Advancedvars/arrays.sh
+++ Advancedvars/arrays.sh
@@ -8,5 +8,10 @@ declare -a indexed_array    # creating an indexed arrar index start from 0
 declare -A associative_array # creation of associative array
 
 # or they may be created using compound assingnments 
-ARRAY=("name" "name2" "name3")
+ARRAY=("name1" "name2" "name3") # this creates an auto indexed array
 
+#-------------------accessing---------------------------------
+# access indexed array elements using index
+echo "First element of indexed array: ${ARRAY[0]}"
+
+# access
