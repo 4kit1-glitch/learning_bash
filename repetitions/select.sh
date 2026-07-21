4f893e8a26d402f6aed7a682d4307ba2deffe519
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-21 09:58:52 +0100

    learn loops

--- repetitions/select.sh
+++ repetitions/select.sh
@@ -1,3 +1,42 @@
 #!/bin/bash 
 
 # this script demonstrates the use of the selcet command to make menus
+
+# Function to display the menu
+show_menu() {
+    echo "Please choose an option:"
+    echo "1. Option 1"
+    echo "2. Option 2"
+    echo "3. Option 3"
+    echo "4. Exit"
+}
+
+# Main loop to handle user input and actions
+while true; do
+    show_menu
+
+    # Read the user's choice
+    read -p "Enter your choice [1-4]: " choice
+
+    case $choice in
+        1)
+            echo "You selected Option 1"
+            ;;
+        2)
+            echo "You selected Option 2"
+            ;;
+        3)
+            echo "You selected Option 3"
+            ;;
+        4)
+            echo "Exiting..."
+            break
+            ;;
+        *)
+            echo "Invalid choice. Please enter a number between 1 and 4."
+            ;;
+    esac
+done
+
+
+## the above can be written in the select more easily
