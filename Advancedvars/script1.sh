23fac5427ac3c1adee72c274d0953615865f857e
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-21 15:27:53 +0100

    learn advanced variables

--- Advancedvars/script1.sh
+++ Advancedvars/script1.sh
@@ -4,4 +4,21 @@
 
 
 # using the declare statement to make variables 
-# syntax 
+# syntax : display -option  var_name=value
+# read man_page to get options
+
+
+## creating a forced interger behavior
+declare -i var=7
+
+echo $var
+
+var="harrison"
+
+echo $var # the result defaults to 0 because it holds just interger values
+
+var=9.89    # error
+
+echo $var
+
+clear
