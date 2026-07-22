0a009c83cfa334e2269f7ece5afb7bdbaeb54eb3
Kengah Ireneaus <4jrkit1@gmail.com>
2026-07-22 04:15:39 +0100

    learn functions

---
+++ functions/functions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# this script demostrates functions
+
+# ----------------------------creations
+function my_func {
+    echo "hello here"
+}
+#or
+my_func2() {
+    echo "bye here"
+}
+
+my_func
+my_func2
